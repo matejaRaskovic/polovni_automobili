6d0de75d27d76b19bdcb54e6f94f6c12c299c3c2
matejaRaskovic <mateja.raskovic@insidemaps.com>
2021-08-21 14:36:58 +0200

    dbg2

--- automobili_dataset.py
+++ automobili_dataset.py
@@ -44,7 +44,7 @@ class CarAdDataset(Dataset):
 
         self.data_info = self.data_info[mask]
 
-        self.data_info.loc[self.data_info['materijal_enterijera'].isin(['Prirodna koža', 'Kombinovana koža'])]['materijal_enterijera'] = 'Koža'
+        self.data_info.loc[self.data_info['materijal_enterijera'].isin(['Prirodna koža', 'Kombinovana koža']), 'materijal_enterijera'] = 'Koža'
         print(self.data_info['materijal_enterijera'].value_counts())
         max_size = self.data_info['materijal_enterijera'].value_counts().max()
         lst = [self.data_info]
