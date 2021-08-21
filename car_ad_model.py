c58c1a8dc8c6c1c527fbf7e7373da5b4a15f3494
matejaRaskovic <mateja.raskovic@insidemaps.com>
2021-08-21 19:39:57 +0200

    dbg 10

--- car_ad_model.py
+++ car_ad_model.py
@@ -79,11 +79,10 @@ class CarAdModel(nn.Module):
         img_sizes = img_sizes.cpu().detach().numpy().astype(int)
         feature_grid = torch.zeros((x.shape[0], 512, 5, 5))
         for i in range(x.shape[0]):
-            print(img_sizes.shape)
-            print(img_sizes[i, 1])
             feature_grid[i:i+1, :] = self.feature_extractor(x[i:i+1, :, 0:img_sizes[i, 1], 0:img_sizes[i, 0]])
 
-        print(feature_grid)
+        feature_grid = feature_grid.view((feature_grid.shape[0], 5*feature_grid.shape[1], feature_grid.shape[2], 1))
+        print(feature_grid.shape)
         # img_features = self.feature_extractor(x)
 
         # print(img_features.shape)
