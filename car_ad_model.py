a3de2067a5d860fa57f18a61f564a3b2d4a2cd1d
matejaRaskovic <mateja.raskovic@insidemaps.com>
2021-08-24 15:38:19 +0200

    a first try

--- car_ad_model.py
+++ car_ad_model.py
@@ -114,7 +114,9 @@ class CarAdModel(nn.Module):
         rnn_output, (ht, ct) = self.rnn_imgs(rnn_input)
         print(rnn_output.shape)
         # print(ht.shape)
-        lin_input = torch.flatten(ht[-1])
+        lin_input = rnn_output.mean(dim=1)
+        lin_input = torch.flatten(lin_input[-1])
+        # lin_input = torch.flatten(ht[-1])
         output = self.linear(lin_input)
 
         output = output.view((10, 10))
