eb9a4105ce24d6ce16942a6ed4e72b3c6bd16c2e
matejaRaskovic <mateja.raskovic@insidemaps.com>
2021-08-26 20:27:49 +0200

    car body dataset

--- getURLS.py
+++ getURLS.py
@@ -52,22 +52,42 @@ brands = ['volkswagen', 'bmw', 'peugeot', 'fiat', 'renault', 'mercedes-benz']
 # URLS_FILE_NAME = "seat_material_dataset.txt"
 
 ################ Dataset for car color ################
-SEARCH_URLS = ["https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=2578&color%5B1%5D=263",
-               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=59",
-               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=261",
-               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=57",
-               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=2573",
-               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=255"]
+# SEARCH_URLS = ["https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=2578&color%5B1%5D=263",
+#                "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=59",
+#                "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=261",
+#                "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=57",
+#                "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=2573",
+#                "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&city_distance=0&showOldNew=all&without_price=1&color%5B0%5D=255"]
+#
+# NUM_ADS = [550,  # narandzasta i zuta
+#            1500,  # crvena
+#            1000,  # zelena
+#            1000,  # plava
+#            4000,  # bela
+#            4000  # crna
+#            ]
+#
+# URLS_FILE_NAME = "car_color_dataset.txt"
 
-NUM_ADS = [550,  # narandzasta i zuta
-           1500,  # crvena
-           1000,  # zelena
-           1000,  # plava
-           4000,  # bela
-           4000  # crna
+################ Dataset for car body ################
+SEARCH_URLS = ["https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&brand=bmw&chassis%5B0%5D=277&city_distance=0&showOldNew=all&without_price=1",
+               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&brand=mercedes-benz&chassis%5B0%5D=277&city_distance=0&showOldNew=all&without_price=1",
+               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&brand=audi&chassis%5B0%5D=277&city_distance=0&showOldNew=all&without_price=1",
+               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&chassis%5B0%5D=278&city_distance=0&showOldNew=all&without_price=1",
+               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&brand=volkswagen&chassis%5B0%5D=2631&city_distance=0&showOldNew=all&without_price=1",
+               "https://www.polovniautomobili.com/auto-oglasi/pretraga?page=1&sort=basic&brand=fiat&model%5B0%5D=125&model%5B1%5D=126&model%5B2%5D=127&model%5B3%5D=128&model%5B4%5D=131&model%5B5%5D=600&model%5B6%5D=850&model%5B7%5D=1100&model%5B8%5D=1107&model%5B9%5D=albea&model%5B10%5D=barchetta&model%5B11%5D=brava&model%5B12%5D=bravo&model%5B13%5D=campagnola&model%5B14%5D=cinquecento&model%5B15%5D=coupe&model%5B16%5D=croma&model%5B17%5D=doblo&model%5B18%5D=evo&model%5B19%5D=fiorino&model%5B20%5D=freemont&model%5B21%5D=fullback&model%5B22%5D=grande-punto&model%5B23%5D=idea&model%5B24%5D=linea&model%5B25%5D=marea&model%5B26%5D=marengo&model%5B27%5D=multipla&model%5B28%5D=palio&model%5B29%5D=punto&model%5B30%5D=qubo&model%5B31%5D=scudo&model%5B32%5D=seicento&model%5B33%5D=spider-europa&model%5B34%5D=stilo&model%5B35%5D=tempra&model%5B36%5D=tipo&model%5B37%5D=ulysse&model%5B38%5D=uno&model%5B39%5D=ostalo&chassis%5B0%5D=2631&city_distance=0&showOldNew=all&without_price=1"]
+
+NUM_ADS = [2000,  # BMW limuzina
+           1500,  # Mercedes limuzina
+           1500,  # Audi limuzina
+           5000,  # karavni svih marki
+           2000,  # VW hecbek
+           1500,  # Peugeot hecbek
+           1500,  # Fiat hecbek - bez Pande i 500 (oni bas i nisu klasicni hecbekovi)
            ]
 
-URLS_FILE_NAME = "car_color_dataset.txt"
+URLS_FILE_NAME = "car_body_dataset.txt"
+
 
 def createHtmlFile(url, html_file="webpage.html"):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
